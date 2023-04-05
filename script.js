686631c592e5a265eda2ee56861b0fdc023b9164
taylaenns8 <taylaenns8@gmail.com>
2023-04-05 16:20:21 -0400

    random colors

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-//change
 
 for (i = 0; i < 4; i++) {
 
     const container = document.createElement("div");
     container.style.display = "flex";
     container.style.flexwrap = "wrap";
-    container.style.width = "800px"
-    container.style.height = "100px"
+    container.style.width = "800px";
+    container.style.height = "100px";
 
     for (let i = 0; i < 8; i++) {
         const tile = document.createElement("div");
@@ -14,11 +13,11 @@ for (i = 0; i < 4; i++) {
         tile.style.paddingBottom = "12.5%";
         tile.style.float = "left";
         
-        if (i % 2 === 0) {
-            tile.style.background = "red";
-        } else {
-            tile.style.background = "black";
-        }
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        
+        tile.style.background = `rgb(${r}, ${g}, ${b})`;
         
         container.append(tile);
     }
@@ -27,8 +26,8 @@ for (i = 0; i < 4; i++) {
     const container2 = document.createElement("div");
     container2.style.display = "flex";
     container2.style.flexwrap = "wrap";
-    container2.style.width = "800px"
-    container2.style.height = "100px"
+    container2.style.width = "800px";
+    container2.style.height = "100px";
 
 
     for (let i = 0; i < 8; i++) {
@@ -37,11 +36,11 @@ for (i = 0; i < 4; i++) {
         tile.style.paddingBottom = "12.5%";
         tile.style.float = "left";
         
-        if (i % 2 === 0) {
-            tile.style.background = "black";
-        } else {
-            tile.style.background = "red";
-        }
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        
+        tile.style.background = `rgb(${r}, ${g}, ${b})`;
         
         container2.append(tile);
     }
